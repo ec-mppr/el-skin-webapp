e5fec60ac624f179e189b6884d36c7fc1317dc83
ELISA CAMILLO <ecamillo@ad.mppr.mp.br>
2025-07-14 16:42:48 -0300

    componentes header e carrossel

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import Carousel from '../components/Carousel/Carousel';
+
+function Home() {
+  return (
+    <>
+      <Header/>
+      <Carousel/>
+    </>
+  );
+}
+
+export default Home;
