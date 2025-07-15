c361752c29648852e369a725d2a3254d5d4b67ab
ELISA CAMILLO <ecamillo@ad.mppr.mp.br>
2025-07-15 16:42:00 -0300

    ajustes - página de contato

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -2,12 +2,46 @@ import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import ImgContact from '../../assets/img_contact.jpg';
 import './Contact.css';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+interface ItemCategoria {
+  title: string
+  key: string
+}
 
 function Contact() {
-  function enviarMensagem(formData: any) {
-    console.log(formData.get('nome'));
+
+  const listaCategoria: ItemCategoria[] = [{
+    title: 'Dúvidas',
+    key: 'duvidas'
+  }, {
+    title: 'Problema com pedido',
+    key: 'problemaPedido'
+  }, {
+    title: 'Reclamação',
+    key: 'reclamacao'
+  }, {
+    title: 'Sugestão',
+    key: 'sugestao'
+  }, {
+    title: 'Elogios',
+    key: 'elogios'
+  }, {
+    title: 'Outro',
+    key: 'outro'
+  }];
+
+  function enviarMensagem(formData: FormData) {
+    const nome = formData.get('nome');
+    const email = formData.get('email');
+    const telefone = formData.get('telefone');
+    const cpf = formData.get('cpf');
+    const mensagem = formData.get('mensagem');
+
+    console.log(nome, email, telefone, cpf, mensagem);
   }
-  
+
   return (
     <>
       <Header />
@@ -17,13 +51,24 @@ function Contact() {
             Fale conosco
           </h2>
           <form action={enviarMensagem} className='form-contact'>
-            <input name="nome" placeholder='Nome' />
-            <input name="email" />
-            <input name="telefone" />
-            <input name="cpf" />
-            <input name="categoria" />
-            <input name="mensagem" />
-            <button type="submit">Enviar mensagem</button>
+            <input className='text-input' name="nome" placeholder='Seu nome' />
+            <input className='text-input' name="email" placeholder='Seu e-mail para contato' />
+            <input className='text-input' name="telefone" placeholder='Seu telefone com DDD' />
+            <input className='text-input' name="cpf" placeholder='Seu CPF' />
+            <p>Categoria:</p>
+            <div className='checkbox-container'>
+              {listaCategoria.map((item) => (
+                <div key={item.key} className='single-checkbox-container'>
+                  <input type='checkbox' className='checkbox' value={item.key} name={item.key} />
+                  <label htmlFor={item.key}>{item.title}</label>
+                </div>
+              ))}
+            </div>
+            <textarea className='text-input' name="mensagem" rows={5} placeholder="Sua mensagem" />
+            <button type="submit" className='button-enviar-mensagem'>
+              <p>Enviar mensagem</p>
+              <FontAwesomeIcon className='icon-enviar-mensagem' icon={faArrowRight} />
+            </button>
           </form>
         </div>
         <div className='column-2'>
