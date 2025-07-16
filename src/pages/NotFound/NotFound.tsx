d40a9b86ed78f8002f222e07e2a8fdfc154bf7ce
ELISA CAMILLO <ecamillo@ad.mppr.mp.br>
2025-07-16 13:25:00 -0300

    corrigindo import não utilizado

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,6 @@
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import './NotFound.css';
-import ImgCover from '../../assets/img_2_carousel.png';
 
 function NotFound() {
   return (
