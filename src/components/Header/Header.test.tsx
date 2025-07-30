dae213c3d365359b8966fb88dcf46d8f22d3a613
ELISA CAMILLO <ecamillo@ad.mppr.mp.br>
2025-07-30 17:41:32 -0300

    Header - testes

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from 'test-utils';
+import Header from './Header';
+import userEvent from '@testing-library/user-event';
+
+test('has search box with empty initial value', async () => {
+  render(<Header/>);
+  const searchInput = screen.getByPlaceholderText('O que você está procurando?');
+  expect(searchInput).toBeInTheDocument(); 
+  expect(searchInput).toHaveValue('');
+});
+
+test('search box allows text input', async () => {
+  const user = userEvent.setup();
+  render(<Header/>);
+  const searchInput = screen.getByPlaceholderText('O que você está procurando?');
+  await user.type(searchInput, 'hidratante');
+  expect(searchInput).toHaveValue('hidratante');
+});
+
+test('submitting search prints the search term to the console', async () => {
+  const user = userEvent.setup();
+  const consoleSpy = jest.spyOn(console, 'log');
+  render(<Header/>);
+  const searchInput = screen.getByPlaceholderText('O que você está procurando?');
+  const searchButton = screen.getByTestId('search-button');
+  const typing = user.type(searchInput, 'hidratante');
+  const clickingButton = user.click(searchButton);
+  Promise.all([typing, clickingButton]).then(() => {
+    expect(consoleSpy).toHaveBeenCalledWith('Você pesquisou por: hidratante');
+  });
+});
+
+test('clicking the cart button shows cart modal', async () => {
+  const user = userEvent.setup();
+  render(<Header />);
+  const cartButton = screen.getByTestId('cart-button');
+  await user.click(cartButton);
+  const carrinho = await screen.findByRole('heading', {name: /carrinho/i});
+  expect(carrinho).toBeInTheDocument();
+});
--- src/components/ProductGrid/ProductGrid.test.tsx
+++ src/components/ProductGrid/ProductGrid.test.tsx
@@ -1,8 +1,10 @@
-import { render, test, screen, fireEvent, act } from 'test-utils';
+import { render, test, screen, fireEvent, act, waitFor } from 'test-utils';
 import ProductGrid from './ProductGrid';
 import { productsMock } from 'mocks/productsMock';
 import { SearchContext } from 'context/SearchContext';
 import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
+import React from 'react';
 
 test('renders full list of products', async () => {
   render(<ProductGrid />);
@@ -20,13 +22,25 @@ test('products are filtered by search term', async () => {
   expect(productsList).toHaveLength(3);
 });
 
+test('message is shown if no product is found with search term', async () => {
+  render(
+    <SearchContext value={{ search: 'olhos', setSearch: () => null }}>
+      <ProductGrid />
+    </SearchContext>
+  );
+
+  expect(screen.queryAllByText(/olhos/i)).toHaveLength(0);
+  expect(screen.getByText(/nenhum produto encontrado/i)).toBeInTheDocument();
+});
+
 test('triggers a function on product click', async () => {
-  const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => undefined);
+  const consoleSpy = jest.spyOn(console, 'log');
   render(<ProductGrid />);
   const productsList = await screen.findAllByTestId('product-card');
   const firstProduct = productsList[0];
   const user = userEvent.setup();
-  user.click(firstProduct).then(() => {
-    expect(consoleSpy).toHaveBeenCalledWith('Produto clicado: 1');
+  await user.click(firstProduct);
+  await waitFor(() => {
+    (expect(consoleSpy).toHaveBeenCalledWith('Produto clicado: 1'));
   });
 });
