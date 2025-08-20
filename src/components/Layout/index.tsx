425cbcc143b849eacae5abc6150f1df508db996a
Elisa Camillo <ecamillo@mppr.mp.br>
2025-08-20 17:43:46 -0300

    atualiza imports

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,21 +3,17 @@ import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 import { Provider } from 'react-redux';
 import { store } from 'store';
-import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'styles/GlobalStyle';
-import { theme } from 'styles/theme';
 
 function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Header />
-          <main>{children}</main>
-          <Footer />
-        </ThemeProvider>
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </Provider>
-      <GlobalStyle theme={theme} />
+      <GlobalStyle />
     </>
   );
 }
--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,9 +1,6 @@
 import { ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
-import { BrowserRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
-import { ThemeProvider } from 'styled-components';
-import { theme } from 'styles/theme';
 import { Provider } from 'react-redux';
 import { store } from 'store';
 
